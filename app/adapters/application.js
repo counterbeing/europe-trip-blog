e9976c9402927f9cff85943feaba676382ecc142
Cory Logan <he@corylogan.com>
2017-06-30 22:44:59 +0200

    change data and euro path around

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,19 +1,18 @@
 import DS from 'ember-data'
 import {pluralize} from 'ember-inflector'
-import config from '../config/environment'
-
+// import config from './config/environment'
 
 export default DS.JSONAPIAdapter.extend({
   namespace: 'data',
   urlForFindAll(modelName) {
     let model = pluralize(modelName)
     let baseUrl = this.buildURL()
-    return `${baseUrl}${config.rootURL}${model}.json`
+    return `${baseUrl}/${model}.json`
   },
 
   urlForFindRecord(id, modelName) {
     let baseUrl = this.buildURL()
     let model = pluralize(modelName)
-    return `${baseUrl}${config.rootURL}/${model}/${id}.json`
+    return `${baseUrl}/${model}/${id}.json`
   },
 })
