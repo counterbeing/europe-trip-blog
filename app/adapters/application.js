9933b72877fa886de557b40e7be3ab4da1060d02
Cory Logan <he@corylogan.com>
2017-08-07 11:45:29 -0400

    adding path joiner

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,18 +1,40 @@
 import DS from 'ember-data'
-import {pluralize} from 'ember-inflector'
-// import config from './config/environment'
+import Ember from 'ember'
+const config = require('../config/environment')
+const { String: { pluralize } } = Ember
+
+let rootURL = config.default.rootURL
 
 export default DS.JSONAPIAdapter.extend({
   namespace: 'data',
-  urlForFindAll(modelName) {
-    let model = pluralize(modelName)
-    let baseUrl = this.buildURL()
-    return `${baseUrl}/${model}.json`
-  },
+  suffix: '.json',
+  buildURL: function(modelName, id, snapshot, requestType, query) {
+    if(id) {
+      let pluralModel = pluralize(modelName)
+      let url = join(rootURL, pluralModel, `${id}.json`)
+      // return `${rootURL}${pluralModel}/${id}.json`
+      return url
+    } else {
+      let prefix = this._super(modelName, id, snapshot, requestType, query)
+      let suffix = this.get('suffix')
+      let combined =  join(rootURL, (prefix + suffix))
+      return combined
+    }
+  }
+});
 
-  urlForFindRecord(id, modelName) {
-    let baseUrl = this.buildURL()
-    let model = pluralize(modelName)
-    return `${baseUrl}/${model}/${id}.json`
-  },
-})
+function join() {
+  var parts = [];
+  for (var i = 0, l = arguments.length; i < l; i++) {
+    parts = parts.concat(arguments[i].split("/"));
+  }
+  var newParts = [];
+  for (i = 0, l = parts.length; i < l; i++) {
+    var part = parts[i];
+    if (!part || part === ".") continue;
+    if (part === "..") newParts.pop();
+    else newParts.push(part);
+  }
+  if (parts[0] === "") newParts.unshift("");
+  return newParts.join("/") || (newParts.length ? "/" : ".");
+}
